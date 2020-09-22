59542e927f55603f23655f3664e3dee1d4b7de5e
WasgtonGomes <71408084+WasgtonGomes@users.noreply.github.com>
2020-09-22 19:54:29 -0400

    Tela de venda

---
+++ tela_venda/src/tela_venda/Tela_venda.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tela_venda;
+
+/**
+ *
+ * @author Wasgton
+ */
+public class Tela_venda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
